7174d288d1dee365ffd7760fac85f95a1dfca4cd
Daniel Sage <dansage@users.noreply.github.com>
2016-09-05 15:33:44 -0500

    Implement the anti-sleep technology

--- Awaken/src/Awaken.cpp
+++ Awaken/src/Awaken.cpp
@@ -35,8 +35,56 @@
 // Include the Windows API headers
 #include <Windows.h>
 
+DWORD WINAPI ThreadProc(LPVOID);
+
 int WINAPI wWinMain(HINSTANCE instance, HINSTANCE, LPTSTR, int show)
 {
+  HANDLE thread = NULL;
+
+  // Create the thread that will keep the system awake
+  if ((thread = ::CreateThread(NULL, NULL, ThreadProc, NULL, NULL, NULL)) == NULL)
+  {
+    // Let the user know that we could not start the thread
+    ::MessageBox(NULL, L"Awaken was unable to start properly. Your system will NOT stay awake!", L"Awaken - Keep your PC awake", MB_ICONERROR | MB_OK);
+    return ::GetLastError();
+  }
+
+  // Let the user know that Awaken has started
+  ::MessageBox(NULL, L"Awaken has started - your system will not enter sleep!", L"Awaken - Keep your PC awake", MB_ICONINFORMATION | MB_OK);
+
+  // Wait for the thread to close (hint: never gonna happen)
+  ::WaitForSingleObject(thread, INFINITE);
+
   // Nothing to see here, quite yet!
+  return S_OK;
+}
+
+DWORD WINAPI ThreadProc(LPVOID)
+{
+  INPUT input = { 0 };
+  MOUSEINPUT mouse = { 0 };
+
+  // Set the event type to "mouse moved"
+  mouse.dwFlags = MOUSEEVENTF_MOVE;
+
+  // Set the input type to "mouse"
+  input.type = INPUT_MOUSE;
+
+  // Point to the mouse input structure
+  input.mi = mouse;
+
+  do
+  {
+    // Prevent Windows from putting the display/system to sleep
+    ::SetThreadExecutionState(ES_DISPLAY_REQUIRED | ES_SYSTEM_REQUIRED);
+
+    // Send a fake mouse movement to prevent screen saver from turning on
+    ::SendInput(1, &input, sizeof(INPUT));
+
+    // Wait for 30 seconds
+    ::Sleep(30000);
+  } while (true);
+
+  // This will never happen:
   return S_OK;
 }
